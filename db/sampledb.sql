f715a0ac2eb949a59eb71f4b10b07645f7cbdba9
gioiediscenna <info@gioiediscenna.com>
2020-05-26 09:54:37 +0200

    dump database

--- db/sampledb.sql
+++ db/sampledb.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE  IF NOT EXISTS `sampledb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 USE `sampledb`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
---
+--6
 -- Host: localhost    Database: sampledb
 -- ------------------------------------------------------
 -- Server version	8.0.15
