53abfa1c89e92c1b938abc36e25ae1438579d4e1
vincenzo85 <you@example.com>
2020-06-09 15:17:50 +0200

    travel

---
+++ src/main/java/it/contrader/controller/TravelController.java
@@ -0,0 +1,80 @@
+package it.contrader.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.contrader.dto.TruckDTO;
+import it.contrader.service.TruckService;
+
+public class TravelController {
+	private String pathFolder = "travel/";
+
+	@Autowired
+	private TruckService service;
+
+	@GetMapping("/getall")
+	public String getAll(HttpServletRequest request) {
+		setAll(request);
+		return pathFolder+"trucks";
+	}
+
+	@GetMapping("/delete")
+	public String delete(HttpServletRequest request, @RequestParam("id") Long id) {
+		service.delete(id);
+		setAll(request);
+		return "trucks";
+	}
+
+	@GetMapping("/preupdate")
+	public String preUpdate(HttpServletRequest request, @RequestParam("id") Long id) {
+		request.getSession().setAttribute("dto", service.read(id));
+		return "updatetruck";
+	}
+
+	@PostMapping("/update")
+	public String update(HttpServletRequest request, 
+			@RequestParam("id") Long id,
+			@RequestParam("howManyPeople") int howManyPeople,
+			@RequestParam("licensePlate") String licensePlate,
+			@RequestParam("peopleBooking") int peopleBooking) {
+
+		TruckDTO dto = new TruckDTO();
+		dto.setId(id);
+		dto.setHowManyPeople(howManyPeople);
+		dto.setLicensePlate(licensePlate);
+		dto.setPeopleBooking(peopleBooking);
+		service.update(dto);
+		setAll(request);
+		return "trucks";
+
+	}
+
+	@PostMapping("/insert")
+	public String insert(HttpServletRequest request,
+			@RequestParam("howManyPeople") int howManyPeople,
+			@RequestParam("licensePlate") String licensePlate,
+			@RequestParam("peopleBooking") int peopleBooking) {
+		TruckDTO dto = new TruckDTO();
+		dto.setHowManyPeople(howManyPeople);
+		dto.setLicensePlate(licensePlate);
+		dto.setPeopleBooking(peopleBooking);
+		service.insert(dto);
+		setAll(request);
+		return "trucks";
+	}
+
+	@GetMapping("/read")
+	public String read(HttpServletRequest request, @RequestParam("id") Long id) {
+		request.getSession().setAttribute("dto", service.read(id));
+		return "readtruck";
+	}
+
+	private void setAll(HttpServletRequest request) {
+		request.getSession().setAttribute("list", service.getAll());
+	}
+
+}
---
+++ src/main/webapp/travel/travel.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Travel Manager</title>
+<!-- Bootstrap core CSS -->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="/css/vittoriostyle.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script>
+  
+</head>
+<body>
+
+
+
+</body>
+</html>
