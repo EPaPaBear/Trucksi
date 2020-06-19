e62749952845e89ff90e3727b25334ceb187ee83
vincenzo85 <you@example.com>
2020-06-19 13:28:42 +0200

    add menu hystorytravel

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -25,8 +25,8 @@ const routes: Routes = [
     path: 'admin-dashboard', component: AdminLayoutComponent, canActivate: [AuthGuard], children: [
       { path: '', component: AdminDashboardComponent },
       { path: 'users', component: UsersComponent },
-      { path: 'work-in-progress', component: WorkInProgressComponent },
-      {path: 'travel', component:TravelComponent}
+      { path: 'work-in-progress', component: WorkInProgressComponent }//,
+     // {path: 'travel', component:TravelComponent}
     ]
   }
 ];
