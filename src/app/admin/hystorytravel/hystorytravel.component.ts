7ab07cfc64b78fbee9a163632a73bc03954736b3
vincenzo85 <you@example.com>
2020-06-22 16:17:10 +0200

    modifiche

--- src/app/admin/hystorytravel/hystorytravel.component.ts
+++ src/app/admin/hystorytravel/hystorytravel.component.ts
@@ -2,36 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractCrudComponent } from 'src/app/utils/abstractcomponent';
 import { HystorytravelDTO } from 'src/dto/hystorytraveldto';
 import { HystorytravelService } from 'src/service/hystorytravel.service';
+import { TEMPLATE_DRIVEN_DIRECTIVES } from '@angular/forms/src/directives';
 
 @Component({
   selector: 'app-hystorytravel',
   templateUrl: './hystorytravel.component.html',
-  styleUrls: ['./hystorytravel.component.css']
+  styleUrls: ['../users/users.component.css']
 })
 export class HystorytravelComponent extends AbstractCrudComponent<HystorytravelDTO> implements OnInit {
-
+  timearrival_s: string;
+  timedeparture_s: string;
   constructor(service: HystorytravelService) {
     super(service);
   }
+  dummyhtdata: HystorytravelDTO[];
 
   ngOnInit() {
     this.clear();
     this.dto = {
-      "id": 1,
-      "nometeam": "eew",
-      "descrizione": "qqq",
-      "numeroutent": "test",
-      "numeroteam": "2",
-      "timedeparture": {
-        'hours': 18,
-        'minutes': 30
+      id: 1,
+      nometeam: 'eew',
+      descrizione: 'qqq',
+      numeroutent: 'test',
+      numeroteam: '2',
+      timedeparture: {
+        hours: 18,
+        minutes: 30
 
       },
-      "timearrival": {
-        'hours': 18,
-        'minutes': 30
+      timearrival: {
+        hours: 19,
+        minutes: 30
       },
-      "truckDTO": null
+      truckDTO: null
 
       /*  "travel": {
             "id": 1,
@@ -39,10 +42,20 @@ export class HystorytravelComponent extends AbstractCrudComponent<HystorytravelD
             "truck": null
         }, */
 
+    };
+    this.dummyhtdata = [ this.dto, this.dto, this.dto ];
+    this.toInputTime(this.dto);
+    for (const dtod of this.dummyhtdata){
+      this.toInputTime(dtod);
+      console.log(dtod);
     }
   }
   clear() {
     this.dto = new HystorytravelDTO();
+  }
+  toInputTime(dto) {
+    dto.timearrival_s = this.dto.timearrival.hours + ':' + this.dto.timearrival.minutes;
+    dto.timedeparture_s = this.dto.timedeparture.hours + ':' + this.dto.timedeparture.minutes;
   }
 
 }
