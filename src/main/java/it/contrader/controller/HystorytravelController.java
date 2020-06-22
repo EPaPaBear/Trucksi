45ddcc64d811cf49f61fa57b1f9034f1d9a5ec7f
vincenzo85 <you@example.com>
2020-06-22 12:18:10 +0200

    ht controller

--- src/main/java/it/contrader/controller/HystorytravelController.java
+++ src/main/java/it/contrader/controller/HystorytravelController.java
@@ -3,6 +3,7 @@ package it.contrader.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import it.contrader.service.HystorytravelService;
 
 @RestController
 @RequestMapping("/hystorytravel")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HystorytravelController  extends AbstractController<HystorytravelDTO> {
 	
 	@Autowired
