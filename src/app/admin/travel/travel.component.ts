02170e2b74e60789dec162054ba5db9e6de8b600
Reuben Mensah <65896757+Reu-Ben@users.noreply.github.com>
2020-06-24 12:36:40 +0200

    No major chngaes

--- src/app/admin/travel/travel.component.ts
+++ src/app/admin/travel/travel.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+ import { Component, OnInit } from '@angular/core';
 import { TruckService } from 'src/service/truck.service';
 import { TravelService } from 'src/service/travel.service';
 import { TravelDTO } from 'src/dto/traveldto';
@@ -7,7 +6,7 @@ import { TruckDTO } from 'src/dto/truckdto';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-travel',
+  selector: 'app-events',
   templateUrl: './travel.component.html',
   styleUrls: ['./travel.component.css'],
   providers: [TravelService, TruckService]
@@ -23,8 +22,8 @@ export class TravelComponent implements OnInit {
   constructor(private truckService: TruckService, private travelService: TravelService, private router: Router) { }
 
   ngOnInit() {
-    this.getTravels();
     this.getTrucks();
+    this.getTravels();
    
   }
 
@@ -38,8 +37,9 @@ export class TravelComponent implements OnInit {
 
 
   insert(travel: TravelDTO){
-    this.truck = this.travelinsert.truck;
-    travel.truck = this.truck;
+    travel.id = 6;
+    this.truck = this.travelinsert.truckDTO;
+    travel.truckDTO = this.truck;
     this.travelService.insert(travel).subscribe(() => this.getTravels());
     this.clear();
   }
@@ -52,12 +52,69 @@ export class TravelComponent implements OnInit {
   
   getTravel(travel: TravelDTO){
     localStorage.setItem('Travel', JSON.stringify(travel));
-  //  this.router.navigate(['/admin-dashboard/notifications']);
+    this.router.navigate(['/admin-dashboard/notifications']);
   }
   
-  
+
   clear(){
     this.travelinsert = new TravelDTO();
   }
-
 }
+
+
+/*
+import { TruckService } from 'src/service/truck.service';
+import { TravelService } from 'src/service/travel.service';
+import { TravelDTO } from 'src/dto/traveldto';
+import { TruckDTO } from 'src/dto/truckdto';
+import { Router } from '@angular/router';
+import { AbstractCrudComponent } from 'src/app/utils/abstractcomponent';
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Come ogni componente di CRUD, questa estende la classe AbstractCrudComponent, ereditando tutti i metodi 
+ * per le CRUD. Questo ci permette di non riscrivere ogni volta gli stessi metodi e avere meno errori.
+ * 
+ * @author Vittorio Valent
+ * 
+ * @see AbstractCrudComponent
+ * 
+ */
+/*
+@Component({
+  selector: 'app-travel',
+  templateUrl: './travel.component.html',
+  styleUrls: ['./travel.component.css'],
+  providers: [TravelService, TruckService]
+})
+
+export class TravelComponent  extends AbstractCrudComponent <TravelDTO> implements OnInit {
+
+  travels: TravelDTO[];
+  trucks: TruckDTO[];
+  travelinsert: TravelDTO = new TravelDTO();
+  truck : TruckDTO;
+
+
+  
+  constructor(travelService: TravelService) {
+    super(travelService);
+  }
+
+  ngOnInit() {
+    this.clear();
+    this.getAll();
+    // Below line extracts all the keys from the enum 
+   // this.userTypeOptions = Object.keys(this.userType).map(key => this.userType[key]).filter(value => typeof value === 'string');
+  }
+
+
+  clear() {
+    this.dto = new TravelDTO();
+
+  }
+  close() {
+    this.selected = null;
+  }
+}
+*/
--- src/dto/traveldto.ts
+++ src/dto/traveldto.ts
@@ -3,5 +3,5 @@ import { TruckDTO } from 'src/dto/truckdto';
 export class TravelDTO {
    id :number;
    date: Date;
-   truck : TruckDTO;
+   truckDTO : TruckDTO;
 }
